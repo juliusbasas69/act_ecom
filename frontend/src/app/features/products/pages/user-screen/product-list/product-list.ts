01d554178d0fb005fa6d9870f856e777b64bc2a1
JuliusB <julius.basas69@gmail.com>
2026-07-13 21:59:11 +0800

    Added category in user products

--- frontend/src/app/features/products/pages/user-screen/product-list/product-list.ts
+++ frontend/src/app/features/products/pages/user-screen/product-list/product-list.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CategoryService } from '../../../../categories/services/category.service';
+import { Category } from '../../../../categories/models/category.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {}
+export class ProductList implements OnInit {
+  private categoryService = inject(CategoryService);
+
+  _categories = signal<Array<Category>>([]);
+
+  ngOnInit(): void {
+    this.categoryService.getAllCategories().subscribe({
+      next: (response) => {
+        const content = response.content;
+        this._categories.set(content);
+        console.log(this._categories());
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+}
